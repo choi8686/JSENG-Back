90d7e572abcf4c35d90ca75b78b7f0b8f3601f17
hwanseog choi <this0924@gmail.com>
2019-07-26 20:11:36 +0900

    config.json update

--- models/index.js
+++ models/index.js
@@ -8,12 +8,11 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
 db.User = require("./auth")(sequelize, Sequelize);
 db.Notice = require("./notice")(sequelize, Sequelize);
@@ -33,7 +32,5 @@ Object.keys(db).forEach(modelName => {
   }
 });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
 
 module.exports = db;
