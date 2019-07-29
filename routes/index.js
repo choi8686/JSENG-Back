59eba991275295be3aac97ca1a91b4b7c5cb95f2
hwanseog choi <this0924@gmail.com>
2019-07-29 15:52:11 +0900

    added router

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ var router = express.Router();
 
 const authRouter = require("../controllers/auth");
 const mainRouter = require("../controllers/main");
+const fileUploadRouter = require("../controllers/upload")
 /* GET home page. */
 router.use("/auth", authRouter);
 router.use("/main", mainRouter);
-
+router.use("/upload", fileUploadRouter)
 module.exports = router;
