81f1a78c76e912595c4e357110ca8b9c8e22b024
hwanseog choi <this0924@gmail.com>
2019-08-01 11:55:43 +0900

    endpoint fix

--- controllers/inquery.js
+++ controllers/inquery.js
@@ -2,9 +2,15 @@ const express = require("express");
 const router = express.Router();
 const nodemailer = require("nodemailer");
 
-app.post("/send", (req, res) => {
-  const { name, company, email, phone, message } = req.body;
-  const output = `
+app.post("", (req, res) => {
+    const {
+        name,
+        company,
+        email,
+        phone,
+        message
+    } = req.body;
+    const output = `
       <p>You have a new contact request</p>
       <h3>Contact Details</h3>
       <ul>  
@@ -17,34 +23,34 @@ app.post("/send", (req, res) => {
       <p>${message}</p>
     `;
 
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    auth: {
-      user: "", // generated ethereal user
-      pass: "" // generated ethereal password
-    },
-    tls: {
-      rejectUnauthorized: false
-    }
-  });
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        auth: {
+            user: "", // generated ethereal user
+            pass: "" // generated ethereal password
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
 
-  // setup email data with unicode symbols
-  let mailOptions = {
-    from: '"Nodemailer Contact" <this0924@email.com>', // sender address
-    to: "this0924@gmail.com", // list of receivers
-    subject: "Node Contact Request", // Subject line
-    text: "Hello world?", // plain text body
-    html: output // html body
-  };
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Nodemailer Contact" <this0924@email.com>', // sender address
+        to: "this0924@gmail.com", // list of receivers
+        subject: "Node Contact Request", // Subject line
+        text: "Hello world?", // plain text body
+        html: output // html body
+    };
 
-  // send mail with defined transport object
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return console.log(error);
-    }
-    console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  });
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log("Message sent: %s", info.messageId);
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    });
 });
 
 module.exports = router;
