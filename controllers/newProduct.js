5a163edf2a5be47db27ee3aaf0c7b9e9c4efbc30
hwanseog choi <this0924@gmail.com>
2019-08-05 12:04:27 +0900

    new product fixing

--- controllers/newProduct.js
+++ controllers/newProduct.js
@@ -1,44 +1,45 @@
 const express = require("express");
 const router = express.Router();
 const models = require("../models");
-
+const path = require("path");
 const AWS = require("aws-sdk");
 const multer = require("multer");
 const multerS3 = require("multer-s3");
 
 AWS.config.loadFromPath(__dirname + "/../config/awsconfig.json");
 const s3 = new AWS.S3();
-const path = require("path");
+
 
 const upload = multer({
     storage: multerS3({
         s3: s3,
 
         bucket: "jseng-image/newProduct", //aws s3에 만든 버킷 이름
+        contentType: multerS3.AUTO_CONTENT_TYPE, // 자동으로 콘텐츠 타입 세팅
         acl: "public-read", //s3 권한
-        metadata(req, file, cb) {
-            cb(null, {
-                fieldName: file.fieldname
-            });
-        },
         key: function (req, file, cb) {
-
-            cb(null, Date.now().toString());
+            console.log(file)
+            cb(null, file.originalname);
         }
-    })
+    }),
+    limits: {
+        fileSize: 5 * 1024 * 1024
+    }
 });
 
 
-router.post('', upload.single('photo'), async (req, res, next) => {
+router.post('', upload.single('img'), async (req, res) => {
     const photoUrl = req.file.location;
     try {
-        const photoUrl = models.newProduct.create({
+        console.log("req.file: ", req.file);
+
+        const imgUpload = models.newProduct.create({
             photoUrl: photoUrl,
             createdAt: new Date(),
             updatedAt: new Date()
         })
         res.status(200).json({
-            photoUrl
+            imgUpload
         });
 
     } catch (error) {
--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,11 @@ var router = express.Router();
 
 const authRouter = require("../controllers/auth");
 const mainRouter = require("../controllers/main");
-const fileUploadRouter = require("../controllers/upload")
 const inqueryRouter = require("../controllers/inquery")
 const newProductRouter = require("../controllers/newProduct")
 /* GET home page. */
 router.use("/auth", authRouter);
 router.use("/main", mainRouter);
-router.use("/upload", fileUploadRouter);
 router.use("/support", inqueryRouter);
 router.use("/newProduct", newProductRouter);
 
