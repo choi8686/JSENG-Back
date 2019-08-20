a903fa1993a8a5124d2c8147976979d1fcdf3eb4
hwanseog choi <this0924@gmail.com>
2019-08-20 16:13:00 +0900

    noticeFile model created

--- models/noticeFile.js
+++ models/noticeFile.js
@@ -1,24 +1,18 @@
 module.exports = (sequelize, DataTypes) => {
-    const notice = sequelize.define(
-        "notice", {
+    const noticeFile = sequelize.define(
+        "noticeFile", {
             id: {
                 type: DataTypes.INTEGER,
                 autoIncrement: true,
                 primaryKey: true
             },
-            title: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            contents: {
+            fileUrl: {
                 type: DataTypes.STRING,
                 allowNull: false
             }
         }, {
-            charset: 'utf8',
-            collate: 'utf8_unicode_ci',
             timestamps: true
         }
     );
-    return notice;
+    return noticeFile;
 };
